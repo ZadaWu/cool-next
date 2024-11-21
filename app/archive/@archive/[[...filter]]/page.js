7b1ebad1b67a515ebc1a8140ccf33e598284f150
zada <zada.wu@gmail.com>
2024-11-21 14:34:29 +0800

    Nested Routes

---
+++ app/archive/@archive/[[...filter]]/page.js
@@ -0,0 +1,41 @@
+import { getAvaiableNewsYears, getNewsByYear, getNewsByYearAndMonth, getAvaiableNewsMonths } from '@/lib/news';
+import Link from 'next/link';
+import NewsList from '@/componenets/news-list';
+
+export default function FilteredNewsPage({ params }) {
+    const { filter } = params;
+    console.log(filter);
+
+    let news;
+    let links = getAvaiableNewsYears(filter);
+
+    const selectedYear = filter?.[0];
+    const selectedMonth = filter?.[1];
+
+    if(selectedYear && !selectedMonth) {
+        news = getNewsByYear(selectedYear);
+        links = getAvaiableNewsMonths(selectedYear);
+        console.log(links);
+    }
+
+    if (selectedMonth && selectedYear) {
+        news = getNewsByYearAndMonth(selectedYear, selectedMonth);
+        links = [];
+    }
+
+    let newsContent = <p>No news found for the selected filter.</p>;
+
+    if(news && news.length > 0) {
+        newsContent = <NewsList news={news} />;
+    }
+    
+    return (
+        <header id="archive-header">
+            <h1>Archive</h1>
+            <ul>
+                {links.map((period) => <li key={period}><Link href={`/archive/${ selectedYear ? selectedYear + '/' : ''}${period}`}>{period}</Link></li>)}
+            </ul>
+            {newsContent}
+        </header>
+    );
+}
