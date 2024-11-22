d7e7eb232ed559e919611c2fde68772ca6f81a39
zada <zada.wu@gmail.com>
2024-11-22 13:23:16 +0800

    直接调用数据库查询

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,14 +1,8 @@
 import NewsList from '@/componenets/news-list';
 import { getAllNews } from '@/lib/news';
 export default async function NewsPage({params}) {
-  // 直接在组件里获取数据，是nextjs的正确的使用方式，页面上会直接有数据生成，有利于SE0） 
-  const response = await fetch('http://localhost:8080/news');
 
-  if (!response.ok) {
-    throw new Error('Failed to fetch news');
-  }
-
-  const news = await response.json();
+  const news = getAllNews();
 
   return (
     <main>
