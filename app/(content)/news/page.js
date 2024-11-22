6d3ef9b21d156a58c8667e23d43d103dbf3f7943
zada <zada.wu@gmail.com>
2024-11-22 13:16:04 +0800

    add backend and call

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,15 +1,20 @@
 import NewsList from '@/componenets/news-list';
 import { getAllNews } from '@/lib/news';
+export default async function NewsPage({params}) {
+  // 直接在组件里获取数据，是nextjs的正确的使用方式，页面上会直接有数据生成，有利于SE0） 
+  const response = await fetch('http://localhost:8080/news');
 
-export default function NewsPage({params}) {
-    const year = params.year;
-    const news = getAllNews(year);
-    console.log('Rendering NewsPage component');
-    return (
-      <main>
-        <h1>News Page</h1>
-        <NewsList news={news} />
-      </main>
+  if (!response.ok) {
+    throw new Error('Failed to fetch news');
+  }
+
+  const news = await response.json();
+
+  return (
+    <main>
+      <h1>News Page</h1>
+      <NewsList news={news} />
+    </main>
     );
 }
   
